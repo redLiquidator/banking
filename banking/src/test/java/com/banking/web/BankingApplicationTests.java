d210a24cedfc87f6429e98b2c00a38de77d82de9
benjamin <inufuni@naver.com>
2023-05-17 22:12:51 -0700

    first commit

---
+++ banking/src/test/java/com/banking/web/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
